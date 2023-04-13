ed0533bd838845b0a106005e06a9f8fdb4948458
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-04-13 16:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your code here
+// Get the header element
+const header = document.querySelector('header');
+
+// Change the text of the h1 element
+header.querySelector('h1').textContent = 'The Dark Knight Rises';
+
+// Get the nav element
+const nav = document.querySelector('nav');
+
+// Add a new list item to the nav element
+const listItem = document.createElement('li');
+const link = document.createElement('a');
+link.href = '#reviews';
+link.textContent = 'Reviews';
+listItem.appendChild(link);
+nav.querySelector('ul').appendChild(listItem);
+
+// Get the summary element
+const summary = document.querySelector('summary');
+
+// Add a click event listener to the summary element
+summary.addEventListener('click', () => {
+  // Toggle the visibility of the actors section
+  const actorsSection = document.querySelector('#actors');
+  actorsSection.style.display = actorsSection.style.display === 'none' ? 'block' : 'none';
+});
+
+// Get all of the blockquote elements
+const blockquotes = document.querySelectorAll('blockquote');
+
+// Loop through each blockquote element and add a click event listener
+blockquotes.forEach(blockquote => {
+  blockquote.addEventListener('click', () => {
+    // Change the text of the blockquote element
+    blockquote.textContent = 'You clicked me!';
+  });
+});
